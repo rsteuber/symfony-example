2aa06b990f83c8d3fd3e777354df7a7cc0ef2480
robsteuber <info@steuberdesign.nl>
2024-05-01 21:47:04 +0200

    Setup Mysql Connection

---
+++ src/Domain/Entity/Party.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Domain\Entity;
+
+use App\Domain\Repository\PartyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PartyRepository::class)]
+class Party
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+}
